6cbe51011565fc69b94049c1a7000cbfc81ccfcf
gkarumbi <gkarumbi@gmail.com>
2020-08-19 02:02:14 +0300

    Add media root and static root settings

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$', views.index, name='index')
+    url('^$', views.index, name='index'),
+    url('project/new/', views.create_project, name = 'new-project'),
+    url('profile/new/', views.create_profile, name = 'new-profile'),
+    url('profiles/upvote/(\d+)', views.like_project, name="like_post"),
+    url('profiles/downvote/(\d+)', views.dislike_project, name="dislike_post"),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
