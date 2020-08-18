735ff7330b2deb286820db97e05a8ec10254eb65
gkarumbi <gkarumbi@gmail.com>
2020-08-18 20:15:39 +0300

    Add profile class to create our profile objects

--- awards/models.py
+++ awards/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
@@ -8,7 +9,8 @@ class Project(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/', blank=True,null=True)
     description = models.TextField()
-    link = UrlField(max_length=200)
+    link = models.URLField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
@@ -20,5 +22,23 @@ class Project(models.Model):
         self.delete()
 
     def update_project(self):
+        self.save()
+
+class Profile(models.Model):
+    profile = models.ImageField(upload_to='profiles/', blank=True,null=True)
+    bio = models.TextField()
+    project = models.ForeignKey(Project, on_delete = models.CASCADE)
+    phone = PhoneNumberField()
+
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(self):
         self.save()
 
