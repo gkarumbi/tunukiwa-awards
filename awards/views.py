e9de96eb1db63059543abb61f60472fd44f6eae3
gkarumbi <gkarumbi@gmail.com>
2020-08-18 21:48:20 +0300

    Add view function to track likes on our projects

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from .models import Project
-from .forms import ProjectForm
+from .models import Project,Profile
+from .forms import ProjectForm,ProfileForm
+from vote.managers import VotableManager
 
-# Create your views here.
+# Create votes object
+votes = VotableManager()
 
 def index(request):
 
@@ -38,3 +40,23 @@ def create_profile(request):
         form = ProfileForm()
 
     return  render(request,"create_profile.html",{'form':form})
+
+
+@login_required(login_url='accounts/login')
+def like_project(request,pk):
+    project = Project.get_single_project(pk)
+    current_user = request.user
+    user_id = current_user.id
+
+    if user.is_authenticated:
+        upvote = project.votes.up(user_id)
+        print(upvote)
+        project.upvote = project.votes.count()
+        project.save()
+
+    return redirect('/')
+
+
+
+
+
