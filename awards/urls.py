7c2065f807344e07c8a0ba37acfb0cd907a530a1
gkarumbi <gkarumbi@gmail.com>
2020-08-20 18:44:00 +0300

    Change profile route

--- awards/urls.py
+++ awards/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     url('^$', views.index, name='index'),
     url('project/new/', views.create_project, name = 'new-project'),
     url('profile/new/', views.create_profile, name = 'new-profile'),
-    url('profiles/(?P<username>[-_\w.]+)$', views.profile, name= 'profiles'),
+    url('accounts/profile/(?P<username>[-_\w.]+)$', views.profile, name= 'profiles'),
     url('profiles/upvote/(\d+)', views.like_project, name="like_post"),
     url('profiles/downvote/(\d+)', views.dislike_project, name="dislike_post"),
     url('api/profile/', views.ProfileList.as_view()),
