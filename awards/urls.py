dd292954ca8dd50653d25073ea73cec6dc06378a
gkarumbi <gkarumbi@gmail.com>
2020-08-20 17:15:58 +0300

    Add url pattern for profile and project api endpoints

--- awards/urls.py
+++ awards/urls.py
@@ -7,9 +7,12 @@ urlpatterns=[
     url('^$', views.index, name='index'),
     url('project/new/', views.create_project, name = 'new-project'),
     url('profile/new/', views.create_profile, name = 'new-profile'),
-    url('profiles/(?P<username>[-_\w.]+)$', views.profile,name='profiles'),
+    url('profiles/(?P<username>[-_\w.]+)$', views.profile, name= 'profiles'),
     url('profiles/upvote/(\d+)', views.like_project, name="like_post"),
     url('profiles/downvote/(\d+)', views.dislike_project, name="dislike_post"),
+    url('api/profile/', views.ProfileList.as_view()),
+    url('api/project/', views.ProjectList.as_view()),
+
 ]
 
 if settings.DEBUG:
